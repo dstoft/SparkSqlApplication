dfdcea6b2f806c6b32e8106f9f104e27f8a073fa
Daniel Skjold Toft <dstoft@hotmail.com>
2020-12-04 18:54:05 +0100

    Attempted Avro reading, unsuccesful so far

--- src/main/java/com/group2/readhdfs/HdfsReadSave.java
+++ src/main/java/com/group2/readhdfs/HdfsReadSave.java
@@ -10,7 +10,11 @@ public class HdfsReadSave {
         SparkSession spark = SparkSession.builder().appName("HdfsReadSave").getOrCreate();
 
         Dataset<Row> rows = spark.read().json("hdfs://10.123.252.244:9000/user/hadoop/twitter-files/coronavirus_tweets_20200127.txt");
-        rows.select("created_at","id","text").limit(10).write().format("text").save("hdfs://10.123.252.244:9000/user/hadoop/twitter-java-save-text");
+
+        // live-twitter-files/FlumeData.1607083399947
+//        Dataset<Row> rows = spark.read().format("org.apache.spark.sql.avro").load("hdfs://10.123.252.244:9000/user/hadoop/live-twitter-files/FlumeData.1607087977897.avro");
+
+        rows.select("created_at","id","text").limit(10).write().format("csv").save("hdfs://10.123.252.244:9000/user/hadoop/twitter-java-save-text");
 
         //spark.readStream().format("parquet").option("inferschema","true").option("maxFilesPerTrigger", "1").option("header", "true").load()
     }
